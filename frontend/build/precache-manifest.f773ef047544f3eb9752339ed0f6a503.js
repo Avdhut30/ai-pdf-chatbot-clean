4f17a09f55d3d01cf227f2e584776850d51e2465
Avdhut Shinde <avdhutshinde86000@gmail.com>
2025-05-05 02:16:37 +0530

    Apply .gitignore and clean up tracked files

---
+++ frontend/build/precache-manifest.f773ef047544f3eb9752339ed0f6a503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac07047324d06f7b995f443df29878f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75bbc264b0de7335d642",
+    "url": "/static/css/main.a57d25d3.chunk.css"
+  },
+  {
+    "revision": "823bf2ada3b7f6c2aa88",
+    "url": "/static/js/2.6f2b4fb8.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.6f2b4fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d217092c058421f822",
+    "url": "/static/js/3.1c0bccc8.chunk.js"
+  },
+  {
+    "revision": "75bbc264b0de7335d642",
+    "url": "/static/js/main.de27779b.chunk.js"
+  },
+  {
+    "revision": "52dd32f74c36fc003772",
+    "url": "/static/js/runtime-main.478fc3ec.js"
+  }
+]);
